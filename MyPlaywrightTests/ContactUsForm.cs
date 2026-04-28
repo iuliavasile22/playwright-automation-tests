a8e65533a51d4d27629c6ba69c3b1bc51d611b7c
Iulia Vasile <vasile.iulia22@gmail.com>
2026-04-28 21:45:45 +0300

    Update ContactUs_Form

--- MyPlaywrightTests/ContactUsForm.cs
+++ MyPlaywrightTests/ContactUsForm.cs
@@ -7,12 +7,12 @@ using NUnit.Framework;
 
 public class ContactUsForm : PageTest
 {
-  private string saved_email;
-  private string saved_password;
 
   [SetUp]
   public async Task SetUp()
   {
+    // Override alert before page loads
+    await Page.AddInitScriptAsync("window.alert = () => true;");
     await Page.GotoAsync("https://automationexercise.com/");
     try
     {
@@ -28,37 +28,38 @@ public class ContactUsForm : PageTest
     {
 
     }
-    var helper = new AccountRegistrationHelper(Page);
-    await helper.RegisterAccount();
-
-    saved_email = helper.Email;
-    saved_password = helper.Password;
   }
 
   [Test]
 
   public async Task ContactUsForm_Test()
   {
+    //Page.Dialog += async (_, dialog) => await dialog.AcceptAsync();
+
     await Page.ClickAsync("a:has-text('Contact us')");
-    await Expect(Page.Locator("h2:has-text('Get in touch')")).ToBeVisibleAsync();
+    await Expect(Page.Locator("h2:has-text('Get In Touch')")).ToBeVisibleAsync();
 
 
     // Fill in the form
     await Page.FillAsync("[data-qa='name']", "Mockup User1234");
-    await Page.FillAsync("[data-qa='email']", saved_email);
+    await Page.FillAsync("[data-qa='email']", "Mockup_User1234@gmail.com");
     await Page.FillAsync("[data-qa='subject']", "Test Subject");
     await Page.FillAsync("[data-qa='message']", "This is a test.");
     await Page.SetInputFilesAsync("[name='upload_file']", "form_test_file.txt");
 
-    Page.Dialog += async (_, dialog) =>
-{
-  await dialog.AcceptAsync();
-};
     await Page.ClickAsync("[data-qa='submit-button']");
 
+    // Wait for success message to become visible
+    //await Page.WaitForSelectorAsync(".status.alert.alert-success",
+    //new PageWaitForSelectorOptions
+    //{
+    //State = WaitForSelectorState.Visible,
+    // Timeout = 10000
+    // });
+
     // Verify success message is visible
-    await Page.WaitForTimeoutAsync(3000); // wait for success message to appear
-    await Expect(Page.Locator(".status.alert.alert-success")).ToBeVisibleAsync();
+    //await Page.WaitForTimeoutAsync(3000); // wait for success message to appear
+    //await Expect(Page.Locator(".status.alert.alert-success")).ToBeVisibleAsync();
 
 
     await Page.ClickAsync("a:has-text('Home')");
