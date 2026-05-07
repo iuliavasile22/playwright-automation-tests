a562066777199154a6c211393c8b0bf51b48d644
Iulia Vasile <vasile.iulia22@gmail.com>
2026-05-07 18:42:46 +0300

    PlaceOrder_RegisterCheckOut creation script

---
+++ MyPlaywrightTests/PlaceOrder_RegisteCheckOut.cs
@@ -0,0 +1,32 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+[TestFixture]
+public class PlaceOrder_RegisterCheckOut : PageTest
+{
+  static PlaceOrder_RegisterCheckOut()
+  {
+    Environment.SetEnvironmentVariable("HEADED", "1");
+  }
+
+  [SetUp]
+  public async Task SetUp()
+  {
+    await Page.AddInitScriptAsync("window.alert = () => true;");
+    await Page.GotoAsync("https://automationexercise.com/");
+    try
+    {
+      await Page.WaitForSelectorAsync(".fc-button.fc-cta-consent.fc-primary-button",
+          new PageWaitForSelectorOptions { Timeout = 4000 });
+      await Page.ClickAsync(".fc-button.fc-cta-consent.fc-primary-button");
+    }
+    catch (TimeoutException) { }
+  }
+
+  [Test]
+  public async Task RegisterCheckOut()
+  {
+
+  }
+}
