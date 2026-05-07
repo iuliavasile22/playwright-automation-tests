b4acaa200e774a725234349d54b0406b1fb28acb
Iulia Vasile <vasile.iulia22@gmail.com>
2026-05-07 19:17:03 +0300

    Update script

--- MyPlaywrightTests/PlaceOrder_RegisteCheckOut.cs
+++ MyPlaywrightTests/PlaceOrder_RegisteCheckOut.cs
@@ -28,5 +28,18 @@ public class PlaceOrder_RegisterCheckOut : PageTest
   public async Task RegisterCheckOut()
   {
 
+    // Navigate to product page
+    await Page.GotoAsync("https://automationexercise.com/product_details/1");
+
+    // Remove ads on the page
+    await Page.EvaluateAsync(@"
+            document.querySelectorAll('ins, .adsbygoogle, iframe[id*=""google""]').forEach(el => el.remove());
+        ");
+
+
+    await Page.Locator("input#quantity").ClearAsync();
+    await Page.Locator("input#quantity").FillAsync("2");
+
+    await Page.ClickAsync("button:has-text('Add to cart')");
   }
 }
