badbcd334589f3fd9c5a3e54359babefa87bcc9c
Iulia Vasile <vasile.iulia22@gmail.com>
2026-05-11 14:44:41 +0300

    View_cart_after_navigation test
    a functional QA test that demonstrates that the quantity cart and prices stay the same after navigating to a different webpage.

--- MyPlaywrightTests/AccountRegistrationHelper.cs
+++ MyPlaywrightTests/AccountRegistrationHelper.cs
@@ -3,39 +3,52 @@ using Microsoft.Playwright;
 public class AccountRegistrationHelper
 {
   private readonly IPage _page;
-  public string Email { get; private set; }
-  public string Password { get; } = "Password13!";
+  public string Email { get; private set; } = string.Empty;
+  public string Password { get; private set; } = string.Empty;
 
   public AccountRegistrationHelper(IPage page)
   {
     _page = page;
-    Email = "mockup_user" + DateTime.Now.Ticks + "@gmail.com";
   }
 
   public async Task RegisterAccount()
   {
+    // Navigate to signup/login page
     await _page.ClickAsync("a:has-text('Signup / Login')");
-    await _page.FillAsync("[data-qa='signup-name']", "mockup user");
+
+    // Fill in name and email
+    Email = "testuser_" + DateTime.Now.Ticks + "@email.com";
+    Password = "TestPass_9x#2026";
+
+    await _page.FillAsync("[data-qa='signup-name']", "Test User");
     await _page.FillAsync("[data-qa='signup-email']", Email);
     await _page.ClickAsync("[data-qa='signup-button']");
+
+    // Fill account information
     await _page.CheckAsync("#id_gender1");
-    await _page.FillAsync("[data-qa='name']", "jane doe");
     await _page.FillAsync("[data-qa='password']", Password);
+
+    // Date of birth
     await _page.SelectOptionAsync("[data-qa='days']", "10");
     await _page.SelectOptionAsync("[data-qa='months']", "4");
     await _page.SelectOptionAsync("[data-qa='years']", "1994");
-    await _page.CheckAsync("input[type='checkbox']:near(:text('Sign up for our newsletter!'))");
-    await _page.CheckAsync("input[type='checkbox']:near(:text('Receive special offers from our partners!'))");
-    await _page.FillAsync("[data-qa='first_name']", "Mockup_name");
+
+    // Fill additional details
+    await _page.FillAsync("[data-qa='first_name']", "Test");
     await _page.FillAsync("[data-qa='last_name']", "User");
-    await _page.FillAsync("[data-qa='company']", "Microsoft");
+    await _page.FillAsync("[data-qa='company']", "Test Company");
     await _page.FillAsync("[data-qa='address']", "123 Test Street");
-    await _page.FillAsync("[data-qa='address2']", "2346 Test Street");
+    await _page.FillAsync("[data-qa='address2']", "456 Test Street");
     await _page.SelectOptionAsync("[data-qa='country']", "India");
-    await _page.FillAsync("[data-qa='state']", "Mockup");
-    await _page.FillAsync("[data-qa='city']", "Mockup");
-    await _page.FillAsync("[data-qa='zipcode']", "10231");
-    await _page.FillAsync("[data-qa='mobile_number']", "1234567891");
+    await _page.FillAsync("[data-qa='state']", "Test State");
+    await _page.FillAsync("[data-qa='city']", "Test City");
+    await _page.FillAsync("[data-qa='zipcode']", "12345");
+    await _page.FillAsync("[data-qa='mobile_number']", "1234567890");
+
+    // Create account
     await _page.ClickAsync("[data-qa='create-account']");
+
+    // Click continue
+    await _page.ClickAsync("[data-qa='continue-button']");
   }
 }
---
+++ MyPlaywrightTests/View_cart_after_navigation.cs
@@ -0,0 +1,98 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+[TestFixture]
+public class View_cart_after_navigation : PageTest
+{
+  private string saved_email = String.Empty;
+  private string saved_password = String.Empty;
+  private bool _accountCreated = false;
+
+  static View_cart_after_navigation()
+  {
+    Environment.SetEnvironmentVariable("HEADED", "1");
+  }
+  private async Task NavigateToHomepage()
+  {
+    await Page.AddInitScriptAsync("window.alert = () => true;");
+    await Page.GotoAsync("https://automationexercise.com/");
+    try
+    {
+      await Page.WaitForSelectorAsync(".fc-button.fc-cta-consent.fc-primary-button",
+          new PageWaitForSelectorOptions { Timeout = 3000 });
+      await Page.ClickAsync(".fc-button.fc-cta-consent.fc-primary-button");
+    }
+    catch (TimeoutException) { }
+  }
+
+  [SetUp]
+  public async Task SetUp()
+  {
+    if (!_accountCreated)
+    {
+      await NavigateToHomepage();
+      var helper = new AccountRegistrationHelper(Page);
+      await helper.RegisterAccount();
+      saved_email = helper.Email;
+      saved_password = helper.Password;
+      _accountCreated = true;
+      await NavigateToHomepage();
+
+      return;
+    }
+    await NavigateToHomepage();
+  }
+
+  private async Task DismissAds()
+  {
+    await Page.EvaluateAsync(@"
+        document.querySelectorAll('ins, .adsbygoogle, iframe[id*=""google""], #google_vignette').forEach(el => el.remove());
+    ");
+    try
+    {
+      await Page.Keyboard.PressAsync("Escape");
+    }
+    catch (Exception) { }
+  }
+
+  [Test]
+  public async Task View_cart_after_navigation_Test()
+  {
+
+    await Expect(Page.Locator("a:has-text('Logged in as')")).ToBeVisibleAsync();
+
+    // Add first product to cart
+    await Page.Locator(".product-image-wrapper").Nth(0).HoverAsync();
+    await Page.ClickAsync(".add-to-cart[data-product-id='1']");
+    await Page.ClickAsync(".modal-footer .btn:has-text('Continue Shopping')");
+
+    // Add second product to cart
+    await Page.Locator(".product-image-wrapper").Nth(1).HoverAsync();
+    await Page.ClickAsync(".add-to-cart[data-product-id='2']");
+
+    await Page.ClickAsync("a[href='/view_cart']");
+
+    await Page.GotoAsync("https://automationexercise.com/products");
+
+    await DismissAds();
+
+    await Page.GotoAsync("https://automationexercise.com/view_cart");
+
+    await Expect(Page.Locator("td.cart_description h4 a:has-text('Blue Top')"))
+       .ToBeVisibleAsync(new() { Timeout = 15000 });
+
+    await Expect(Page.Locator("td.cart_description a:has-text('Men Tshirt')"))
+        .ToBeVisibleAsync(new() { Timeout = 15000 });
+
+    // Verify prices, quantity and total
+    await Expect(Page.Locator("td.cart_price").Nth(0)).ToContainTextAsync("Rs. 500");
+    await Expect(Page.Locator("td.cart_quantity").Nth(0)).ToContainTextAsync("1");
+    await Expect(Page.Locator("td.cart_total").Nth(0)).ToContainTextAsync("Rs. 500");
+
+    await Expect(Page.Locator("td.cart_price").Nth(1)).ToContainTextAsync("Rs. 400");
+    await Expect(Page.Locator("td.cart_quantity").Nth(1)).ToContainTextAsync("1");
+    await Expect(Page.Locator("td.cart_total").Nth(1)).ToContainTextAsync("Rs. 400");
+  }
+}
